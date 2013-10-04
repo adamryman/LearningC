b676c56180cadbb424356e95f518b0d7c4f269d2
Adam Ryman <adamryman@gmail.com>
2013-10-04 11:06:07 -0400

    Init Commit; Hello, World!

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  LearningC
+//
+//  Created by Adam Ryman on 10/4/13.
+//  Copyright (c) 2013 Adam Ryman. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+

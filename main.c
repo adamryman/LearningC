898d63677d02c0f34669a3850c82d73d165144f3
Adam Ryman <adamryman@gmail.com>
2013-10-04 13:14:32 -0400

    Adding prototype for method pointerTest

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 int j, k;
 int *ptr;
 
-
+int pointerTest();
 
 int main(int argc, const char * argv[])
 {
@@ -25,7 +25,9 @@ int pointerTest(){
     k = 2;
     ptr = &k;
     printf("\n");
-    printf("j has the value %d and is stored at %p\n", j, (void *)&j); printf("k has the value %d and is stored at %p\n", k, (void *)&k); printf("ptr has the value %p and is stored at %p\n", ptr, (void *)&ptr);
+    printf("j has the value %d and is stored at %p\n", j, (void *)&j);
+    printf("k has the value %d and is stored at %p\n", k, (void *)&k);
+    printf("ptr has the value %p and is stored at %p\n", ptr, (void *)&ptr);
     printf("The value of the integer pointed to by ptr is %d\n", *ptr);
     return 0;
 }
